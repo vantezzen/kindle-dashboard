1919af83e7b1e14b8665e6e389d783bcbd516347
Bennett <hello@vantezzen.io>
2026-08-16 12:52:38 +0200

    dev

--- components/dashboard/countdown.tsx
+++ components/dashboard/countdown.tsx
@@ -19,7 +19,7 @@ export function Countdown({ now }: CountdownProps) {
         {dayName}
       </div>
       <div className="text-[20px] font-bold">
-        {days} days, {hours}:{minutes}h
+        {days} days, {hours}h, {minutes}min
       </div>
     </div>
   );
